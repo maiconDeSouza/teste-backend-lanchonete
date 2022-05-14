8ce3d054914ad4100bb60d81a786aab403de9876
Maicon Souza <maicon_souza09@hotmail.com>
2022-05-14 15:20:03 -0300

    finalizando a api

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 
 const routerAdmin = require('./routes/admin')
 const routerCustomer = require('./routes/customer')
 
+app.use(cors({
+    origin: 'http://127.0.0.1:5500'
+}))
 
 app.use(express.json())
 app.use('/admin',routerAdmin)
